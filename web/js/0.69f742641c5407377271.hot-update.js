9c1b56184b0bcdfb52d0554ed5ff35a44dcbbe6f
JEC <jec@g-p-i.fr>
2016-12-07 15:31:54 +0100

    feat(twig) admin,home,twig,config,favison

---
+++ web/js/0.69f742641c5407377271.hot-update.js
@@ -0,0 +1,11 @@
+webpackHotUpdate(0,{
+
+/***/ 3:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue__ = __webpack_require__(0);\n/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_vue__);\n\n\nnew __WEBPACK_IMPORTED_MODULE_0_vue___default.a({\n  el: \"#vuejs_app\",\n  components: {}\n});\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9DOi9MYWIvUGVyc29ubmVsL2dvb2dsZXBsYXlfY29tbWVudC9zcmMtdnVlanMvc3JjL21haW4uanM/MmIwZiJdLCJuYW1lcyI6WyJlbCIsImNvbXBvbmVudHMiXSwibWFwcGluZ3MiOiI7O0FBQUE7O0FBRUEsSUFBSSwyQ0FBSixDQUFRO0FBQ05BLE1BQUksWUFERTtBQUVOQyxjQUFZO0FBRk4sQ0FBUiIsImZpbGUiOiIzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFZ1ZSBmcm9tICd2dWUnO1xyXG5cclxubmV3IFZ1ZSh7XHJcbiAgZWw6IFwiI3Z1ZWpzX2FwcFwiLFxyXG4gIGNvbXBvbmVudHM6IHtcclxuXHJcbiAgfVxyXG59KTtcblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gLi9DOi9MYWIvUGVyc29ubmVsL2dvb2dsZXBsYXlfY29tbWVudC9zcmMtdnVlanMvc3JjL21haW4uanMiXSwic291cmNlUm9vdCI6IiJ9");
+
+/***/ }
+
+})
